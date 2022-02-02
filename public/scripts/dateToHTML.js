2a58551d647b34e89a4d0caa6511347505c3eb4d
Justin <justin1998m1@gmail.com>
2022-02-01 20:13:19 -0500

    Checks for valid date before parsing the strings now

--- public/scripts/dateToHTML.js
+++ public/scripts/dateToHTML.js
@@ -1,10 +1,13 @@
 var dateToHTML = function(date){
-
-  var month = String(date.getMonth() + 1)
-  var day = String(date.getDate())
-  var year = String(date.getFullYear())
-  return month +  '-' + day + '-' + year
-
+  if(date){
+    var month = String(date.getMonth() + 1)
+    var day = String(date.getDate())
+    var year = String(date.getFullYear())
+    return month +  '-' + day + '-' + year
+  }
+//display sort function may pass empty date var and this false tell the display sort
+//function the date doesnt exist and not to display it
+return false
 }
 
 export default dateToHTML
