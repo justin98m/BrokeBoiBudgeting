3b94c97d4f287f9cbc9669adec7f821a0a24a28e
Justin <justin1998m1@gmail.com>
2022-02-01 20:14:48 -0500

    adjusted sort to account for table w/ or w/o certain attributes

--- public/scripts/sort.js
+++ public/scripts/sort.js
@@ -2,13 +2,13 @@ import toDateObject from './toDateObject.js'
 import bubblesort from './bubblesort.js'
 import displaySort from './displaySort.js'
 
+var defaultSort = {column : "date",  direction : "ascend" }
 var htmlTable = document.getElementsByClassName('row')
 let sortBar = document.getElementById('sortBar')
 //default for both buttons, changing of id  after load wont change the actual node
 let ascend = document.getElementById('active')
 let descend = document.getElementById('inactive')
 let rowData = storeRowData()
-const defaultSort = {column : "date",  direction : "ascend" }
 let sortBy = defaultSort
 
 callBubblsort(defaultSort)
@@ -54,22 +54,34 @@ function callBubblsort(sortBy){
 //put html table information in to an array of objects containing all column info
 function storeRowData(){
   var rowData = new Array()
-  var type,tableRow
+  var type,tableRow,date,id
   for (var i = 0; i <htmlTable.length; i++) {
     //a copy is made so when values of htmlTable are written over the
     //row Data values wont change, specifically classList
     tableRow = htmlTable[i].cloneNode(true)
+    //some function callers will not have a row with a date
+    if (tableRow.querySelector(".date") !== null) {
+      date = toDateObject(tableRow.querySelector(".date").innerHTML)
+    }
+    else {
+      defaultSort.column = "name"
+      date = false
+    }
+    if(tableRow.querySelector(".id") !== null){
+      id = tableRow.querySelector(".id").outerHTML
+    }
+    else{id = false}
+
     rowData[i] = {
-      date: toDateObject(tableRow.querySelector(".date").innerHTML),
-      id: tableRow.id,
+      date: date,
       //this helps determine how to display the value
-      //if classList[1] doesnt exist nothing happens
-      //type: htmlTable[i].querySelector(".dollars").classList[1],
       classList: tableRow.querySelector(".dollars").classList,
       //get dollar string, remove dollar sign and turn into a number
       dollars: parseInt(tableRow.querySelector(".dollars").innerHTML.slice(1)),
-      name: tableRow.querySelector(".name").innerHTML
+      name: tableRow.querySelector(".name").innerHTML,
+      id : id
     }
+    console.log("The Id: ",rowData[i].id);
   }
   return rowData
 }
