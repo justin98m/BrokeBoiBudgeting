7eab6d76caa4b2cc1548a2ac3f3311f097152307
Justin <justin1998m1@gmail.com>
2022-02-01 20:19:22 -0500

    just changes a var name fundIds now equals fundId

--- scripts/consolidateFundIds.js
+++ scripts/consolidateFundIds.js
@@ -1,22 +1,23 @@
 var fundIds = function(category){
 
 //The first fundid is set manually because
-//it ensures nested loop runs and is gurenteed to be a unique ID
-fundIds = [category[0].fundId]
+//it ensures nested loop runs and must be a unique ID
+var fundId = [category[0].fundId]
 //loop compiles all the unique fundids associate with all categorys pulled
 for (var i = 0; i < category.length; i++) {
-    for (var n = 0; n < fundIds.length; n++) {
-      //fundId already exist in list so skip to the next category
-      if(category[i].fundId == fundIds[n]){
+    for (var n = 0; n < fundId.length; n++) {
+      //this fundId already exist in list so skip to the next category
+      if(category[i].fundId == fundId[n]){
         //console.log(category[i].fundId," already exist in list: ", fundIds);
         break
       }
-      else if(n == fundIds.length -1) {
+      else if(n == fundId.length -1) {
           //same as pushing the unique id into the array
-          fundIds[fundIds.length] = category[i].fundId
+          fundId[fundId.length] = category[i].fundId
       }
     }
   }
-  return fundIds
+  console.log("Fund IDS:", fundId);
+  return fundId
 }
 exports.fundIds = fundIds
